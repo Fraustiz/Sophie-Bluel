e88d2600092bfc434d8d8da2c4839099b9ae5fdc
Jules <jules.drevon@gmail.com>
2023-05-29 02:47:54 +0200

    commit

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -4,27 +4,31 @@ loginForm.addEventListener('submit', async (event) => {
     const email = document.querySelector('#email').value;
     const password = document.querySelector('#password').value;
     const wrongCombinaison = document.querySelector(".wrong");
-    const response = await fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password
-        })
-    });
-    const data = await response.json();
-    if (response.ok) {
-        localStorage.setItem('email', email);
-        localStorage.setItem('password', password);
-        localStorage.setItem('token', data.token);
-        window.location.href = 'index.html';
-    } else {
-        wrongCombinaison.classList.remove("wrong-animate");
-        setTimeout(function() {
-            wrongCombinaison.classList.add("wrong-animate");
-        }, 10);
-        wrongCombinaison.innerHTML = "<p>Email ou Mot de passe incorrect.</p>";
-    }
+        const response = await fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            const tokenExpiration = new Date();
+            tokenExpiration.setTime(tokenExpiration.getTime() + (24 * 60 * 60 * 1000)); // 24 hours in milliseconds
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('tokenExpiration', tokenExpiration.getTime());
+            window.location.href = 'index.html';
+        } else {
+            wrongCombinaison.classList.remove("wrong-animate");
+            setTimeout(function() {
+                wrongCombinaison.classList.add("wrong-animate");
+            }, 10);
+            wrongCombinaison.innerHTML = "<p>Email ou Mot de passe incorrect.</p>";
+        }
+
+    login(email, password);
 });
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,25 +1,76 @@
 const categoriesContainer = document.querySelector("#categories");
 const worksContainer = document.querySelector(".gallery");
 
-const categoriesData = sessionStorage.getItem('categoriesData');
-const worksData = sessionStorage.getItem('worksData');
-
-if (categoriesData && worksData) {
-    const categories = JSON.parse(categoriesData);
-    const works = JSON.parse(worksData);
-    render(categories, works);
-} else {
+function refreshList() {
     fetch('http://localhost:5678/api/categories')
     .then(response => response.json())
     .then(categories => {
         fetch('http://localhost:5678/api/works')
         .then(response => response.json())
         .then(works => {
-        sessionStorage.setItem('categoriesData', JSON.stringify(categories));
-        sessionStorage.setItem('worksData', JSON.stringify(works));
-        render(categories, works);
+            render(categories, works);
         });
     });
+}
+
+function refreshListWorks() {
+    fetch('http://localhost:5678/api/works')
+    .then(response => response.json())
+    .then(works => {
+        renders(works);
+    });
+}
+
+refreshList();
+
+function renders(works) {
+    const worksContainers = document.querySelector(".work-edit");
+    const articles = works.map(work => `<figure class="work" data-id="${work.id}"><div class="work-btn"><i class="fa-solid fa-arrows-up-down-left-right"></i><i class="fa-solid fa-trash-can"></i></div><img src="${work.imageUrl}"></figure>`).join("");
+    worksContainers.innerHTML = articles;
+
+    const worksArrows = document.querySelectorAll('.work');
+    worksArrows.forEach(work => {
+        const arrows = work.querySelectorAll('.fa-arrows-up-down-left-right');
+        work.addEventListener('mouseenter', () => {
+            arrows.forEach(arrow => {
+                arrow.style.display = 'flex';
+            });
+        });
+        work.addEventListener('mouseleave', () => {
+            arrows.forEach(arrow => {
+                arrow.style.display = 'none';
+            });
+        });
+    });
+
+    const deleteWork = document.querySelectorAll('.work-edit .work');
+    deleteWork.forEach(async work => {
+        const trash = work.querySelector('.fa-trash-can');
+        if (trash) {
+            const dataId = work.dataset.id;
+
+            trash.addEventListener('click', async () => {
+                const token = localStorage.getItem('token');
+
+                fetch(`http://localhost:5678/api/works/${dataId}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                .then(
+                    refreshList(),
+                    refreshListWorks()
+                );
+            });
+        }
+    });
+}
+
+function backToNormal() {
+    const backToNormal = document.querySelector(".replace-to-img");
+    backToNormal.innerHTML = "<i class='fa-regular fa-image'></i><button>+ Ajouter photo</button><p>jpg, png : 4mo max</p>";
+    backToNormal.style.padding = "28px 0 19px 0";
 }
 
 function render(categories, works) {
@@ -36,10 +87,6 @@ function render(categories, works) {
 
     const articles = works.map(work => `<figure class="work" data-id="${work.category.id}"><img src="${work.imageUrl}"><figcaption>${work.title}</figcaption></figure>`).join("");
     worksContainer.innerHTML = articles;
-    setInterval(() => {
-        sessionStorage.removeItem('categoriesData');
-        sessionStorage.removeItem('worksData');
-    }, 3600000);
 }
 
 function updateGallery(categoryId) {
@@ -92,21 +139,6 @@ function getToken() {
                 modalDisplay[i].style.display = "flex";
                 document.body.style.overflow = "hidden";
             }
-
-            const works = document.querySelectorAll('.work');
-            works.forEach(work => {
-            const arrows = work.querySelectorAll('.fa-solid.fa-arrows-up-down-left-right');
-            work.addEventListener('mouseenter', () => {
-                arrows.forEach(arrow => {
-                arrow.style.display = 'flex';
-                });
-            });
-            work.addEventListener('mouseleave', () => {
-                arrows.forEach(arrow => {
-                arrow.style.display = 'none';
-                });
-            });
-            });
         });
 
         
@@ -120,21 +152,20 @@ function getToken() {
         const closeEditWorks = document.querySelectorAll(".modal, .close");
         for (let i = 0; i < closeEditWorks.length; i++) {
             closeEditWorks[i].addEventListener("click", function() {
-                location.reload()
+                let modalNone = document.querySelector(".modal-content");
+                let modalAdd = document.querySelector(".modal-content-add");
+                let modal = document.querySelector(".modal");
+                modalNone.style.display = "none";
+                modalAdd.style.display = "none";
+                modal.style.display = "none";
+                document.body.style.overflow = "auto";
+                backToNormal();
+                const form = document.querySelector('.form-add-img');
+                form.reset();
             });
         }
 
-        fetch('http://localhost:5678/api/works')
-        .then(response => response.json())
-        .then(works => {
-            renders(works);
-        });
-
-        const worksContainers = document.querySelector(".work-edit");
-        function renders(works) {
-            const articles = works.map(work => `<figure class="work" data-id="${work.category.id}"><div class="work-btn"><i class="fa-solid fa-arrows-up-down-left-right"></i><i class="fa-solid fa-trash-can"></i></div><img src="${work.imageUrl}"></figure>`).join("");
-            worksContainers.innerHTML = articles;
-        }
+        refreshListWorks();
 
         const addWork = document.querySelector(".modal-content button");
         addWork.addEventListener("click", function(event) {
@@ -147,6 +178,9 @@ function getToken() {
             backBtn.addEventListener("click", () => {
                 modalAdd.style.display = "none";
                 modalNone.style.display = "flex";
+                backToNormal();
+                const form = document.querySelector('.form-add-img');
+                form.reset();
             });
 
 
@@ -157,7 +191,7 @@ function getToken() {
             });
 
             function renderSelectOptions(categories) {
-                const selectCategory = document.querySelector("#select_category");
+                const selectCategory = document.querySelector("#category");
                 const options = categories.map(category => `<option value="${category.id}">${category.name}</option>`).join("");
                 selectCategory.innerHTML = `<option value="" disabled selected style="display:none;"></option>${options}`;
             }
@@ -174,6 +208,7 @@ function getToken() {
                 const [file] = imgInp.files;
                 if (file) {
                     let imageAdd = document.querySelector(".add-img");
+                    let imageReplace = document.querySelector(".replace-to-img");
                     let imgElement = document.createElement("img");
                     imgElement.src = URL.createObjectURL(file);
                     imgElement.onload = function() {
@@ -184,17 +219,17 @@ function getToken() {
                         let ratio = Math.min(parentWidth/width, parentHeight/height);
                         imgElement.style.width = `${ratio * width}px`;
                         imgElement.style.height = `${ratio * height}px`;
-                        imageAdd.innerHTML = "";
-                        imageAdd.appendChild(imgElement);
-                        let divPadding = document.querySelector(".add-img");
+                        imageReplace.innerHTML = "";
+                        imageReplace.appendChild(imgElement);
+                        let divPadding = document.querySelector(".replace-to-img");
                         divPadding.style.padding = "0px";
                     };
                 }
             }
 
             const addImageInput2 = document.querySelector('#imgInp');
-            const workNameInput = document.querySelector('input[name="work_name"]');
-            const workCategorySelect = document.querySelector('select[name="work_category"]');
+            const workNameInput = document.querySelector('input[name="title"]');
+            const workCategorySelect = document.querySelector('select[name="category"]');
             const submitButton = document.querySelector('input[type="submit"]');
 
             addImageInput2.addEventListener('change', () => {
@@ -216,43 +251,54 @@ function getToken() {
                     submitButton.setAttribute('disabled', '');
                 }
             }
-
-            const form = document.querySelector('.form-add-img');
-
-            form.addEventListener('submit', function(event) {
-                event.preventDefault();
-                
-                function addWork(event) {
-                    event.preventDefault();
-                    
-                    const form = event.target;
-                    const formData = new FormData(form);
-                    const token = localStorage.getItem('token');
-
-                    fetch('http://localhost:5678/api/works', {
-                    method: 'POST',
-                    body: formData,
-                    headers: {
-                        'Authorization': `Bearer ${token}`
-                    }
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                    console.log('Success:', data);
-                    form.reset();
-                    updateGallery();
-                    })
-                    .catch(error => {
-                    console.error('Error:', error);
-                    });
-                }
-                
-                addWork(event);
-            });
         });
     }
 }
 
 window.addEventListener('load', function() {
+
+    const form = document.querySelector('.form-add-img');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        async function addWork(event) {
+            const token = localStorage.getItem("token");
+            const form = event.target;
+            const formData = new FormData(form);
+
+            fetch('http://localhost:5678/api/works', {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(data => {
+                form.reset();
+                refreshList();
+                refreshListWorks();
+                const modalClear = document.querySelectorAll(".modal-content-add, .modal");
+                for (const modal of modalClear) {
+                    modal.style.display = "none";
+                }
+                document.body.style.overflow = "auto";
+                backToNormal();
+            })
+        }
+        
+        addWork(event);
+    });
+
     getToken();
+    const storedToken = localStorage.getItem('token');
+    const storedExpiration = localStorage.getItem('tokenExpiration');
+
+    if (storedToken && storedExpiration) {
+        const now = new Date().getTime();
+        if (now > storedExpiration) {
+            localStorage.removeItem('token');
+            localStorage.removeItem('tokenExpiration');
+        }
+    }
 });
